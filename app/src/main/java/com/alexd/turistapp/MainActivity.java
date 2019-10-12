ee23d09a27ba8adaa8449c2e7fea519f924e668e
AlexDss3 <aehv545@gmail.com>
2019-10-11 23:04:10 -0600

    5th Commit

--- app/src/main/java/com/alexd/turistapp/MainActivity.java
+++ app/src/main/java/com/alexd/turistapp/MainActivity.java
@@ -1,15 +1,23 @@
 package com.alexd.turistapp;
 
+import androidx.annotation.IdRes;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
     }
+
 }
--- app/src/main/java/com/alexd/turistapp/SplashScreen.java
+++ app/src/main/java/com/alexd/turistapp/SplashScreen.java
@@ -19,6 +19,7 @@ public class SplashScreen extends AppCompatActivity {
             public void run() {
                 Intent intent = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(intent);
+                finish();
             }
         }, 2000);
     }
